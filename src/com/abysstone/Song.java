c2c879558337691e8dd86d987b4d35fe9c98cf7e
abbystone <manasbhatt007@gmail.com>
2018-04-16 03:18:12 +0530

    commit 0 post full completion

---
+++ src/com/abysstone/Song.java
@@ -0,0 +1,23 @@
+package com.abysstone;
+
+/**
+ * Created by abysstone on 12/12/17.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
